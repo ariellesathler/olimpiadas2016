09c573d514ac68d1cecd34e2254e6fa07288008b
Sheyla Stefhania <sheylastefhania@gmail.com>
2016-03-16 00:05:35 -0300

    Criação das páginas BH e LOGIN

---
+++ belohorizonte.php
@@ -0,0 +1,46 @@
+<h1>Belo Horizonte</h1>
+
+<div>
+    <h2>Sobre</h2>
+    <p>
+        Belo Horizonte é a moderna capital de Minas Gerais fundada em 12 de dezembro de 1897 atualmente é a 5ª mais populosa cidade brasileira. A cidade é mundialmente conhecida e exerce significativa influência nacional e até internacional, seja do ponto de vista cultural, econômico ou político. 
+    </p>
+
+    <h2>Minerão</h2>
+
+    <p>
+        Construído em 1965 e frequentemente utilizado para grandes competições nacionais e internacionais de futebol, o Mineirão foi totalmente reformado para a Copa do Mundo FIFA 2014 e tem capacidade bruta para 69.900 espectadores.. O estádio fica localizado próximo à Lagoa da Pampulha e a diversos monumentos projetados pelo arquiteto Oscar Niemeyer – juntas, as atrações compõem uma das regiões mais belas da cidade de Belo Horizonte.
+    </p>
+    
+    <h2>Pontos Turísticos</h2>
+    <p>
+      Belo Horizonte conta com importantes monumentos, parques e museus, como o Museu de Arte da Pampulha, o Museu de Artes e Ofícios, o Museu de Ciências Naturais da PUC Minas, o Circuito Cultural Praça da Liberdade, o Conjunto Arquitetônico da Pampulha, o Mercado Central e a Savassi, e eventos de grande repercussão, como o Festival Internacional de Teatro, Palco e Rua (FIT-BH), Festival Internacional de Curtas e o Encontro Internacional de Literaturas em Língua Portuguesa.
+    </p>
+
+    <h2>Hóteis</h2>
+    <p>
+      Allia Gran Hotel Pampulha Suites
+      Rua Intendente Câmara, 540
+      Pampulha
+      31 3311-0100
+      0800 283 9988
+      http://www.alliahotels.com.br/allia/belo-horizonte/allia-gran-hotel-pampulha-suites
+
+      Bristol Alameda Business Hotel
+      Avenida das Palmeiras, 743
+      Pampulha, São Luís
+      31 2522-4300
+      http://www.bristolhoteis.com.br/br/hotel/lazer.aspx?NumHotel=50
+
+      Bristol Jaraguá Hotel
+      Rua Boaventura, 987
+      Pampulha, Jaraguá
+      31 3232-9700
+      0800 283 9988
+      http://www.alliahotels.com.br/bristol/belo-horizonte/jaragua-hotel
+    </p>
+    
+
+    
+
+</div>
