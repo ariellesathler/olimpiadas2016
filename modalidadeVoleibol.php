54d01c89cf6adf4aeb5486598cc1652d447992c3
unknown <sheyla@ATLAS.piovesan.lan>
2016-03-18 14:05:08 -0300

    Inserção de conteúdo

---
+++ modalidadeVoleibol.php
@@ -0,0 +1,32 @@
+<h1>Vôlei de Praia</h1>
+
+<div class="texto-modalidade">
+  <h2>Finalidade</h2>
+  <p>
+    Em uma quadra de areia dividida por uma rede, duas duplas se enfrentam, com o objetivo de fazer com que a bola toque a quadra do adversário
+  </p>
+
+  <h2>Composição da equipe</h2>
+
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at rhoncus urna. Morbi rhoncus mauris ligula, et faucibus erat pellentesque sit amet. Aenean mattis risus non quam eleifend, sit amet aliquet nulla commodo. Vestibulum a mauris et nisi rutrum dictum. Praesent blandit justo risus, vulputate commodo lectus tincidunt id. In luctus ultricies turpis. Curabitur nulla est, convallis eget iaculis nec, facilisis a est. Donec felis purus, malesuada eu fringilla sit amet, semper ut mauris. Cras malesuada massa eget arcu varius, nec egestas massa aliquet. Proin consequat leo nisl, a scelerisque enim ornare id.
+  </p>
+
+  <h2>Regras</h2>
+
+  <p>
+    Sed bibendum id ipsum et consectetur. Etiam dictum orci ac neque egestas rutrum. Morbi venenatis blandit diam, nec varius dolor efficitur et. Fusce molestie erat vehicula vehicula porta. Nulla facilisi. Sed at justo scelerisque, pretium elit eget, sollicitudin diam. Quisque eget turpis venenatis metus cursus dignissim. Mauris efficitur posuere massa suscipit varius. Vivamus sapien ipsum, gravida et ante nec, fringilla tincidunt ligula. Phasellus pulvinar mauris non augue condimentum, quis fringilla velit porttitor. Proin eget nisl ut eros ultricies vehicula. Nulla vitae ultricies augue. Duis sodales pretium metus dapibus volutpat. Aenean feugiat sem odio, in facilisis ipsum tempus sit amet.vehicula. Nulla vitae ultricies augue. Duis sodales pretium metus dapibus volutpat. Aenean feugiat sem odio, in facilisis ipsum tempus sit amet.
+  </p>
+
+  <h2>Estréia nos Jogos Olímpicos</h2>
+
+  <p>
+    Atlanta 1996
+  </p>
+
+  <h2>Provas</h2>
+  <p>
+    Masculina e feminina
+  </p>
+
+</div>
